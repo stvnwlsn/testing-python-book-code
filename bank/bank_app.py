62926aa629347fc75659b7726a8523fed133a0c4
Steven Wilson <stvn.wlsn@gmail.com>
2014-07-14 14:49:37 +0100

    use cProfile and pycallgraph

--- bank/bank_app.py
+++ bank/bank_app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template, request
+
+from bank.account import Account
 from bank.bank import Bank
 
 
@@ -15,4 +17,8 @@ def hello_world():
     return render_template ('index.html', balance=balance)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import cProfile
+
+    account = Account('1111', 50)
+    BANK.add_account(account)
+    cProfile.run('app.run(debug=True)', sort='time')
